41cfee9862677bf60ee5ec33c9e0bc23efa38f24
SkyDanBinVan <daniel.hope@sky.uk>
2022-03-10 14:34:58 +0000

    Initiated some basic components.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <>
+        <header>
+            <a href=""><img className="logo" src="/logo192.png" alt="loho" /></a>
+            <h1>Food App</h1>
+        </header>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Hero.css"
+
+
+
+function Main() {
+    return (
+        <div className='hero'>
+        </div>
+    )
+}
+
+export default Main
