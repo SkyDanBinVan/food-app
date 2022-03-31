852706d89427ac303d5f4a1045c93d50d3f71987
SkyDanBinVan <daniel.hope@sky.uk>
2022-03-31 10:52:17 +0100

    Added bootstrap Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css';
 import Layout from "./layout/Layout/Layout"
 import Hero from "./components/Hero/Hero"
 import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Foodcards from './components/FoodCards/Foodcards';
 function App() {
   return (
     <>
-            <Layout>  
-                <Routes>
-                    {/* <Route path="/" element={<Hero />}></Route> */}
-                </Routes>
-            </Layout>
+      <Layout>  
+          <Routes>
+              <Route path="/" element={<Hero />}></Route>
+              <Route path="/cards" element={<Foodcards />}></Route>
+          </Routes>
+      </Layout>
     </>
   );
 }
